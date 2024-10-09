db79fdd4c32e6fcd56e3e60ec1162b054541f46f
blact <1664102260@qq.com>
2024-10-09 14:25:18 +0800

    change the output filename

--- rule.py
+++ rule.py
@@ -192,8 +192,7 @@ def main(rules,person_info):
     timestamp = time.strftime("%Y%m%d%H%M%S",localT)
 
     #rules = psk_rules(rules_file=rules,person_file=person_info,save_file="/opt/tools/il/out/"+timestamp+".txt")
-    #change the output filename
-    rules = psk_rules(rules_file=rules,person_file=person_info,save_file="/opt/tools/il/out/"+person_info[:-5]+".txt")
+    rules = psk_rules(rules_file=rules,person_file=person_info,save_file="/opt/tools/il/out/"+os.path.basename(person_info)[:-5]+".txt")
     rules._result.get_result()
     
 
